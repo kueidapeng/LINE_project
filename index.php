c30921a8b822efedec300ee947e284392cfd8460
Vito <LinWei.Chen@acer.com>
2017-11-27 18:23:57 +0800

    remove secret

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
  
  $bot = new \LINE\LINEBot(
-  new \LINE\LINEBot\HTTPClient\CurlHTTPClient('nWO1xSc7dTcojx0ztJQxqkQwf/xSHLihc5RQI1tyjtS8fTDVUiUD8eSjvmGDWqxWZvQOnCHbLVLlEYB4qf+UUiuCUN2XNOwE7rNl6PpawiZCSFUSSoDS8qv6ijh25Apl5EY+xRk7RudlgNoPFgtJwgdB04t89/1O/w1cDnyilFU='),
-  ['channelSecret' => '23a6bd00bb961c5e2764ca2d9ca37860']
+  new \LINE\LINEBot\HTTPClient\CurlHTTPClient(''),
+  ['channelSecret' => '']
 );
  
 $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
