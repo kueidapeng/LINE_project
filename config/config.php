bd17e4938c9c61ef9b3060d8ebdab6e0f1a6ff1c
Vito <LinWei.Chen@acer.com>
2017-11-28 14:28:30 +0800

    config

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$config = [
+		'bot_setting' => [
+				'curlHTTPClient' => 'nWO1xSc7dTcojx0ztJQxqkQwf/xSHLihc5RQI1tyjtS8fTDVUiUD8eSjvmGDWqxWZvQOnCHbLVLlEYB4qf+UUiuCUN2XNOwE7rNl6PpawiZCSFUSSoDS8qv6ijh25Apl5EY+xRk7RudlgNoPFgtJwgdB04t89/1O/w1cDnyilFU='
+				, 'channelSecret' => '23a6bd00bb961c5e2764ca2d9ca37860'
+		]
+	];
+?>
