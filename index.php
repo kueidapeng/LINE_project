7a507549a3b5ff9a32861e4f5e1d11ae8bac8cae
DarrenPeng <Darren.Peng@acer.com>
2017-11-28 10:38:31 +0800

    #update index.php

--- index.php
+++ index.php
@@ -8,17 +8,31 @@ require_once __DIR__ . '/vendor/autoload.php';
 );
  
 $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-
-
-
+ 
 $body = file_get_contents("php://input");
     $events = $bot->parseEventRequest($body, $signature);
     foreach ($events as $event) {
-        if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
-            $reply_token = $event->getReplyToken();
-
-    $bot->replyText($reply_token, "11111");
+		
+        if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) { //return為文字訊息
+            
+			$reply_token = $event->getReplyToken();
+			$getText = $event->getText();
+			
+			//單筆傳送
+			/*
+			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($getText); //文字
+			$stickerMessageBuilder = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1,17); //貼圖
+			
+			$response = $bot->replyMessage($reply_token, $stickerMessageBuilder);
+			$response =  $bot->replyMessage($reply_token, $textMessageBuilder);
+			*/
+			
+			$MultiMessageBuilder = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
+			 
+			$MultiMessageBuilder->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($getText)); 
+			$MultiMessageBuilder->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1,17)); 
+			
+			$bot->replyMessage($reply_token, $MultiMessageBuilder);
         }
     }
  
