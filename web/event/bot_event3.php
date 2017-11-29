623eae9892babc8b84ee6102d69fca7aba853fa8
DarrenPeng <Darren.Peng@acer.com>
2017-11-29 15:11:04 +0800

    #update bot_event3.php test

---
+++ web/event/bot_event3.php
@@ -0,0 +1,12 @@
+﻿ 
+<?php
+			$originalContentUrl="https://4.share.photo.xuite.net/min0427/140d536/6411593/262027757_x.jpg";
+			$previewImageUrl="https://4.share.photo.xuite.net/min0427/140d536/6411593/262027757_x.jpg";
+ 
+			$ImageMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalContentUrl,$previewImageUrl); //文字
+ 
+			$response =  $bot->replyMessage($reply_token, $ImageMessageBuilder);
+ 
+
+?>
+ 
