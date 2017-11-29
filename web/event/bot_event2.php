e01e2124d3f636f4f77c41cfa83db3cc7ab5fded
DarrenPeng <Darren.Peng@acer.com>
2017-11-29 15:54:51 +0800

    #update test

--- web/event/bot_event2.php
+++ web/event/bot_event2.php
@@ -4,18 +4,18 @@
  
  
 			 $columns = array();
-			$img_url = "圖片網址，必需為 https (圖片非必填欄位)";
-			for($i=0;$i<5;$i++) //最多5筆
-			{
+			$img_url = "https://4.share.photo.xuite.net/min0427/140d536/6411593/262027757_x.jpg";
+	 
+
 			  $actions = array(
 				//一般訊息型 action
 				new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("按鈕1","文字1"),
 				//網址型 action
 				new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("觀看食記","http://www.google.com")
 			  );
-			  $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("標題".$i, "說明".$i, $img_url , $actions);
-			  $columns[] = $column;
-			}
+			  $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("標題1", "說明1", $img_url , $actions);
+
+	
 			$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
 			$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $carousel);
 			$bot->replyMessage($reply_token,$msg);
