ea4a94215ab68fcb8d8ec2f270ed086e0f0641a9
DarrenPeng <Darren.Peng@acer.com>
2017-11-29 15:45:54 +0800

    #update bot_event2.php test

--- web/event/bot_event2.php
+++ web/event/bot_event2.php
@@ -2,9 +2,20 @@
 <?php
 
 		 
-			$stickerMessageBuilder = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1,15); //貼圖
+			$actions = array(
 			
-			$response = $bot->replyMessage($reply_token, $stickerMessageBuilder);
+			  new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("按鈕1","文字1"),
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("Google","http://www.google.com"),
+			  
+			  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("下一頁", "page=3"),
+			  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("上一頁", "page=1")
+			);
+			 
+			$img_url = "圖片網址，必需為 https (圖片非必填欄位)";
+			$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("按鈕文字","說明", $img_url, $actions);
+			$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
+			$bot->replyMessage($reply_token,$msg);
+ 
  
  
 
