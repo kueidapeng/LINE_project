686dd64da7f836748287d98cfc4fb9f2d8877b69
DarrenPeng <Darren.Peng@acer.com>
2017-11-29 15:59:25 +0800

    #update

--- web/event/bot_event2.php
+++ web/event/bot_event2.php
@@ -2,23 +2,20 @@
 <?php
 
  
- 
-			 $columns = array();
-			$img_url = "https://4.share.photo.xuite.net/min0427/140d536/6411593/262027757_x.jpg";
-	 
-
-			  $actions = array(
-				//一般訊息型 action
-				new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("按鈕1","文字1"),
-				//網址型 action
-				new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("觀看食記","http://www.google.com")
-			  );
-			  $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("標題1", "說明1", $img_url , $actions);
-
-	
-			$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
-			$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $carousel);
-			$bot->replyMessage($reply_token,$msg);
+		$actions = array(
+		  //一般訊息型 action
+		  new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("按鈕1","文字1"),
+		  //網址型 action
+		  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("Google","http://www.google.com"),
+		  //下列兩筆均為互動型action
+		  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("下一頁", "page=3"),
+		  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("上一頁", "page=1")
+		);
+		 
+		$img_url = "https://4.share.photo.xuite.net/min0427/140d536/6411593/262027757_x.jpg";
+		$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("按鈕文字","說明", $img_url, $actions);
+		$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
+		$bot->replyMessage($reply_token,$msg);
 
 ?>
  
