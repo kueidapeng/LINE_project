4a1692fd24758c8916e62e6e285f88cdab6d974a
Vito <LinWei.Chen@acer.com>
2017-11-29 17:21:07 +0800

    modify  index for template

--- web/event/bot_carousel.php
+++ web/event/bot_carousel.php
@@ -1,10 +1,19 @@
 <?php
-        $actions = array(
-            new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("是", "ans=Y"),
-            new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("否", "ans=N")
-          );
-          $button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder("問題", $actions);
-          $msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
-          $bot->replyMessage($replyToken,$msg);
+        $columns = array();
+		$img_url = "https://github.com/chliwei199/images/raw/master/str1.png";
+		for($i=0;$i<5;$i++) //最多5筆
+		{
+		  $actions = array(
+			//一般訊息型 action
+			new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("按鈕1","文字1"),
+			//網址型 action
+			new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("觀看食記","http://www.google.com")
+		  );
+		  $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("標題".$i, "說明".$i, $img_url , $actions);
+		  $columns[] = $column;
+		}
+		$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
+		$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $carousel);
+        $bot->replyMessage($reply_token,$msg);
         
 ?>
