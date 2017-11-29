19659ae7e8e57685a858591d13325d25d831f65d
DarrenPeng <Darren.Peng@acer.com>
2017-11-29 14:04:28 +0800

    #update no_event.php

--- web/event/no_event.php
+++ web/event/no_event.php
@@ -1,6 +1,6 @@
 ﻿ 
 <?php
-			$textMessage = '\uDBC0\uDC84';
+			$textMessage = '\x{1F622} 很抱歉，我不知道你在說什麼。\x{1F623}';
  			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textMessage);  
 			$response =  $bot->replyMessage($reply_token, $textMessageBuilder);
 
