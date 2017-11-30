3a21120d6b192e06f799baceeb987ea3c74f79e5
DarrenPeng <Darren.Peng@acer.com>
2017-11-30 16:36:12 +0800

    #update imagemap test

--- web/event/bot_imagemap.php
+++ web/event/bot_imagemap.php
@@ -2,16 +2,25 @@
 <?php
 
 		    $columns = array();
-			$baseUrl="https://4.share.photo.xuite.net/min0427/140d536/6411593/262027757_x.jpg";
+			$baseUrl="https://raw.githubusercontent.com/chliwei199/images/master/cube.png";
 			$altText="方塊圖片";
 			
-			$baseSizeBuilder = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder(1040,1040);
-			$areaBuilder = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(0,0,520,1040);
+			$baseSizeBuilder = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder(500,500);
+			$areaBuilderLT = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(0,0,250,250); //1
+			$areaBuilderRT = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(250,0,250,250);//2
+			$areaBuilderLD = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(0,-250,250,250);//3
+			$areaBuilderRD = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(-250,-250,250,250);//4
 			
-			$linkUri='https://www.google.com.tw/';
 			
- 
-			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder($linkUri,$areaBuilder);
+			$text1="https://www.google.com.tw/";
+			$text2="https://www.google.com.tw/";			
+			$linkUri1="https://www.google.com.tw/";
+			$linkUri2="https://www.google.com.tw/";
+			
+			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($text1,$areaBuilderLT);
+			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder($linkUri1,$areaBuilderRT);
+			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($text2,$areaBuilderLD);
+			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder($linkUri2,$areaBuilderRD);
 
  
 			$ImageMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder($baseUrl,$altText,$baseSizeBuilder,$columns);  
