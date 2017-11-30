42cee35382066b811cea94823c881d127b9316a4
DarrenPeng <Darren.Peng@acer.com>
2017-11-30 16:55:09 +0800

    #update imagemap test

--- web/event/bot_imagemap.php
+++ web/event/bot_imagemap.php
@@ -2,7 +2,7 @@
 <?php
 
 		    $columns = array();
-			$baseUrl="https://github.com/chliwei199/images/raw/master/2.jpg";
+			$baseUrl="https://4.share.photo.xuite.net/min0427/140d536/6411593/262027757_x.jpg";
 			$altText="方塊圖片";
 			
 			$baseSizeBuilder = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder(1040,1040);
@@ -12,12 +12,12 @@
 			$areaBuilderRD = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(520,520,520,520);//4
 			
 			
-			//$text1="block1";
+			 $text1="block1";
 			//$text2="block2";			
 			$linkUri1="https://www.google.com.tw/";
 			//$linkUri2="https://www.youtube.com/";
 			
-			//$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($text1,$areaBuilderLT);
+			 $columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($text1,$areaBuilderLT);
 			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder($linkUri1,$areaBuilderRT);
 			//$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($text2,$areaBuilderLD);
 			//$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder($linkUri2,$areaBuilderRD);
