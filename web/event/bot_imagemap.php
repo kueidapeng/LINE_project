bfc959f4324e6938c1f021cd6742e6f1fe58ef91
DarrenPeng <Darren.Peng@acer.com>
2017-11-30 17:19:36 +0800

    #update imagemap test

--- web/event/bot_imagemap.php
+++ web/event/bot_imagemap.php
@@ -2,7 +2,7 @@
 <?php
 
 		    $columns = array();
-			$baseUrl="https://4.share.photo.xuite.net/min0427/140d536/6411593/262027757_x.jpg";
+			$baseUrl="https://raw.githubusercontent.com/chliwei199/images/master/cube.png";
 			$altText="方塊圖片";
 			
 			$baseSizeBuilder = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder(1040,1040);
@@ -12,15 +12,15 @@
 			$areaBuilderRD = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(520,520,520,520);//4
 			
 			
-			 $text1="block1";
-			 $text2="block2";			
+			$text1="LINE在2016年9月底的「LINE Developer Day 2016」大會上發表了功能性高、且可提供開發人員豐富支援的Messaging API功能。有開發能力的服務提供商即可透過Messaging API將自己的服務內容串聯到LINE@上。對這些服務提供商的好友來說，即可透過LINE聊天介面與該LINE@帳號進行雙向互動，並操作各式線上服務，享用完整、一致的使用者體驗。";
+			$text2="你可以在LINE@帳號上開啟Messaging API功能，以打造專屬的LINE聊天機器人。";			
 			$linkUri1="https://www.google.com.tw/";
-			 $linkUri2="https://www.youtube.com/";
+			$linkUri2="https://www.youtube.com/";
 			
-			 $columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($text1,$areaBuilderLT);
+			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($text1,$areaBuilderLT);
 			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder($linkUri1,$areaBuilderRT);
-			 $columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($text2,$areaBuilderLD);
-			 $columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder($linkUri2,$areaBuilderRD);
+			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($text2,$areaBuilderLD);
+			$columns[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder($linkUri2,$areaBuilderRD);
 
  
 			$ImageMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder($baseUrl,$altText,$baseSizeBuilder,$columns);  
