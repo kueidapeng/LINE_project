af3599d648d7ff21fa000a852328ee7d39b5545d
DarrenPeng <Darren.Peng@acer.com>
2017-11-30 16:51:30 +0800

    #update imagemap test

--- web/event/bot_imagemap.php
+++ web/event/bot_imagemap.php
@@ -5,11 +5,11 @@
 			$baseUrl="https://github.com/chliwei199/images/raw/master/2.jpg";
 			$altText="方塊圖片";
 			
-			$baseSizeBuilder = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder(500,500);
-			$areaBuilderLT = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(0,0,250,250); //1
-			$areaBuilderRT = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(250,0,250,250);//2
-			$areaBuilderLD = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(0,250,250,250);//3
-			$areaBuilderRD = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(250,250,250,250);//4
+			$baseSizeBuilder = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder(1040,1040);
+			$areaBuilderLT = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(0,0,520,520); //1
+			$areaBuilderRT = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(520,0,520,520);//2
+			$areaBuilderLD = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(0,520,520,520);//3
+			$areaBuilderRD = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(520,520,520,520);//4
 			
 			
 			//$text1="block1";
