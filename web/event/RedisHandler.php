a3b68aa385b9c33331c339dffec9d16bb1b682a1
Vito <LinWei.Chen@acer.com>
2017-12-12 18:09:01 +0800

    modify RedisHandler for checkUser

--- web/event/RedisHandler.php
+++ web/event/RedisHandler.php
@@ -1,24 +1,47 @@
 <?php
+namespace App\event;
+
+use App\event\UtilityHandler;
+use Predis\Autoloader as PredisAutoloader;
+
 class RedisHandler {
  
      private $redis;
 
     public function __construct()
     {
-       Predis\Autoloader::register();
-       $this->redis = new Predis\Client(getenv('REDIS_URL'));
+        PredisAutoloader::register();
+       $this->redis = new \Predis\Client(getenv('REDIS_URL'));
     }
-    public function checkUserId($userId)
+    public function addUserId($user_id)
     {
-     //    return $userId;
-     // $test=$this->redis->exists('test') ? "Oui" : "please populate the message key";
-     $test=$this->redis->SETNX($userId,$userId);
-     return $test;
+      $result= UtilityHandler::toBoolean($this->redis->hset($user_id,'user_id',$user_id));
+      return $result;
     }
-    // public function checkUserLocation($userId,$latitude,$longitude)
-    // {
-    // $test=$this->redis->SETNX($userId,$userId);
-    //  return $test;
-    // }
+    public function deleteUserId($user_id)
+    {
+      $result= UtilityHandler::toBoolean($this->redis->del($user_id));
+      return $result;
+    }
+    public function checkUserId($user_id)
+    {
+      $result= UtilityHandler::toBoolean($this->redis->exists($user_id));
+      return $result;
+    }
+    public function checkUserLocation($user_id,$latitude,$longitude)
+    {
+        $user=$this->redis->hgetall($user_id);
+        if(empty($user))
+        {
+          return UtilityHandler::toBoolean(0);
+        }
+        if(isset($user['latitude']) && isset($user['longitude'])){
+            $result=1;        
+         }else{
+            $result=0;        
+         }
+        return UtilityHandler::toBoolean($result);
+    }
+    
    
 }
