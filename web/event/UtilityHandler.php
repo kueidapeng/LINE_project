43ac11163742e838864a4749ffdb97b7fcfb0464
Vito <LinWei.Chen@acer.com>
2017-12-30 01:32:45 +0800

    add getJsonString and emoji function

--- web/event/UtilityHandler.php
+++ web/event/UtilityHandler.php
@@ -8,5 +8,23 @@ class UtilityHandler {
 	 		return false;
 	 	return ($value == 0) ? false : true;
 	 }
-   
+	 public function getJsonString() {
+		$stream = fopen('description.json', 'r');
+		$listener = new \JsonStreamingParser\Listener\InMemoryListener();
+		try {
+ 			 $parser = new \JsonStreamingParser\Parser($stream, $listener);
+  			 $parser->parse();
+  			fclose($stream);
+			} catch (Exception $e) {
+ 		 	fclose($stream);
+  			throw $e;
+		}
+		return $listener->getJson();
+	}
+	public function emoji($ID){
+		$bin = hex2bin(str_repeat('0', 8 - strlen($ID)) . $ID);
+	   $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
+	   return $emoticon;
+	}
+
 }
