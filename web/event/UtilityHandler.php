86b6b713a57c3d080875bfcc595e29d66f06b981
Vito <LinWei.Chen@acer.com>
2017-12-30 01:51:45 +0800

    add tag_translate funcation

--- web/event/UtilityHandler.php
+++ web/event/UtilityHandler.php
@@ -26,5 +26,22 @@ class UtilityHandler {
 	   $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
 	   return $emoticon;
 	}
+	public function tag_translate($ori_string){
+		$pattern_angle = "/\{{(.*?)\}}/"; // remain{{}}
+		$pattern_square = "/\{{(.*)\s\w+='(.*)'\}}/"; // deal with{{...}}
+													  // Tag content_og_tag																
+		// Tag content
+		preg_match_all($pattern_angle, $ori_string, $matches_angle);
+		foreach ($matches_angle[0] as $angle) {
+			preg_match_all($pattern_square, $angle, $matches);
+			if (isset($matches[0][0])) {
+				$be_replaced[] = $matches[0][0]; // save string be replace
+				$replacement[] = $this->{$matches[1][0]}($matches[2][0]); // save replace string
+			}
+		}
+		if (isset($be_replaced) && isset($replacement))
+		$Text = str_replace($be_replaced, $replacement, trim($ori_string));
+	   return $Text;
+	}
 
 }
