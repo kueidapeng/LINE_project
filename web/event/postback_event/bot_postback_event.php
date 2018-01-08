92e4fd6e085965d18f567b64dd2517152f52c8a5
Vito <LinWei.Chen@acer.com>
2018-01-08 16:33:05 +0800

    modify description and bot_postback_event for return text

--- web/event/postback_event/bot_postback_event.php
+++ web/event/postback_event/bot_postback_event.php
@@ -1,8 +1,11 @@
 <?php
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 			/*
 			post event
 			*/
-			
+			$UtilityHandler= new App\event\UtilityHandler;				//create UtilityHandler object
+			$jsonString=$UtilityHandler->getJsonString();				//get json string from description
+
 			$getText = $event->getPostbackData();
 			
 			if ($getText) {
@@ -13,9 +16,11 @@
 
 					switch ($page) {
 						case 'Y':
+						$bot->pushMessage($user_id, new TextMessageBuilder($jsonString['bot_postback_event_continue_search'])); //message push
 						include('event/message_event/bot_map_search.php');
 						break;
 						case 'N':
+						$bot->pushMessage($user_id, new TextMessageBuilder($jsonString['bot_postback_event_cardhoin_service'])); //message push
 						include('event/message_event/bot_imagemap.php');
 						break;				
 					}
@@ -24,12 +29,14 @@
 				if (isset($data["map_cat"])) {
 					
 					$page = $data["map_cat"];
-
+						
 					switch ($page) {
 						case 'Y':
+						$bot->pushMessage($user_id, new TextMessageBuilder($jsonString['bot_postback_event_continue_search'])); //message push
 						include('event/message_event/bot_map_search.php');
 						break;
 						case 'N':
+						$bot->pushMessage($user_id, new TextMessageBuilder($jsonString['bot_postback_event_cardhoin_service'])); //message push
 						include('event/message_event/bot_imagemap.php');
 						break;				
 					}
