e769c2e43e2ce2ed1165b666e0f9c30c9f0d58be
Vito <LinWei.Chen@acer.com>
2018-01-12 16:01:28 +0800

    modify for ImagemapHandler

--- web/event/message_event/bot_map_search.php
+++ web/event/message_event/bot_map_search.php
@@ -15,10 +15,8 @@
 				$originalContentUrl='https://'. $_SERVER['HTTP_HOST'].'/line_bot/image/location_desc.png';
 				$previewImageUrl='https://'. $_SERVER['HTTP_HOST'].'/line_bot/image/location_desc.png';
 
-
 				$actions = array(
- 
-						   new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder(emoji('1F4CD')." 定位座標", "line://nv/location")
+ 						   new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder(emoji('1F4CD')." 定位座標", "line://nv/location")
 						   );
 								   
 					
@@ -28,10 +26,8 @@
 				$MultiMessageBuilder = new LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
 		
 				$ImageMessageBuilder = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalContentUrl,$previewImageUrl); //文字
-				//$Text = new LINE\LINEBot\MessageBuilder\TextMessageBuilder("定位完成以後請再次點選定定位收尋(若想更改座標重新定位)。".emoji('100033'));
+			
 				$MultiMessageBuilder->add($msg);
-				//$MultiMessageBuilder->add($ImageMessageBuilder);
-				//$MultiMessageBuilder->add($Text);
 				$bot->replyMessage($reply_token, $MultiMessageBuilder);
 		
 	}else{
@@ -41,55 +37,17 @@
 		$latlng=$redis->getLocation($user_id);
 		$zip_code =$redis->getzipcode($user_id);
 
-
 		$contents = json_decode(file_get_contents("https://www.cardhoin.com/apiserver/deviceapi/v1/geoes/live?zip_code=".$zip_code))->result->geo003->geo;
 		
 		$area_level_1=$contents->parent->value;
 		$area_level_2=$contents->value;
 
 	   $actions = array(
- 
-		//new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder(emoji('1F50D')." 類別搜尋", "map_cat=Y"),
-		//new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder(emoji('1F50D')." 關鍵字搜尋", "map_key=Y"),	
 	    new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder(emoji('1F4CD')." 更新位置", "line://nv/location")
 		);
 
-		$baseUrl='https://'. $_SERVER['HTTP_HOST'].getenv('image_path').'cat_menu_hand.png?_ignore=';
-		
-				   $altText="卡好用類別";
-		
-				   $baseSizeBuilder = new LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder(750,1040);
-				   $areaBuilderKeyword = new LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(17,46,944,113); 	//LT block
-				   $areaBuilderCat1 = new LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(84,211,174,174);	//RT block
-				   $areaBuilderCat2 = new LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(319,211,174,174);	//LD block
-				   $areaBuilderCat3 = new LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(551,211,174,174);	//RD block
-				   $areaBuilderCat4 = new LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(786,211,174,174);	//RD block
-				   $areaBuilderCat5 = new LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(84,467,174,174);	//RD block
-				   $areaBuilderCat6 = new LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(319,467,174,174);	//RD block
-				   $areaBuilderCat7 = new LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(551,467,174,174);	//RD block
-				   
-				   $linkUri1="關鍵字搜尋";
-				   $Cat1="類別：美味食光";
-				   $Cat2="類別：購物優惠";
-				   $Cat3="類別：影藝娛樂";
-				   $Cat4="類別：旅遊住宿";
-				   $Cat5="類別：加油租車";
-				   $Cat6="類別：交通運輸";
-				   $Cat7="類別：網路平台";
-		
-				   $columns[] = new LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($linkUri1,$areaBuilderKeyword);
-				   $columns[] = new LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($Cat1,$areaBuilderCat1);
-				   $columns[] = new LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($Cat2,$areaBuilderCat2);
-				   $columns[] = new LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($Cat3,$areaBuilderCat3);
-				   $columns[] = new LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($Cat4,$areaBuilderCat4);
-				   $columns[] = new LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($Cat5,$areaBuilderCat5);
-				   $columns[] = new LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($Cat6,$areaBuilderCat6);
-				   $columns[] = new LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder($Cat7,$areaBuilderCat7);
-	   
-				   $ImageMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder($baseUrl,$altText,$baseSizeBuilder,$columns);  
-				   
-				   
-				
+		$imagemap= new App\event\message_event\ImagemapHandler;				//create imagemap object
+		$ImageMessageBuilder=$imagemap->createImagemap();
  
 	$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder(emoji('1F50D')." 優惠搜尋","目前定位點： ".$area_level_1." ➤ ".$area_level_2."\n若要更改地點請點選更新位置。", $thumbnailImageUrl,$actions);
 	$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(emoji('1F50D')."這訊息要在手機上才能看唷", $carousel);
@@ -98,7 +56,5 @@
 	$MultiMessageBuilder->add($ImageMessageBuilder);
 
 	$bot->replyMessage($reply_token,$MultiMessageBuilder);	
- 
-
-	}
+ 	}
 ?>
