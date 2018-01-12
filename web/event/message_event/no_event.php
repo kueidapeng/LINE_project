291250ea304455083d629975a46c2f6fde3f9f12
Vito <LinWei.Chen@acer.com>
2018-01-12 23:23:23 +0800

    modify no_event description

--- web/event/message_event/no_event.php
+++ web/event/message_event/no_event.php
@@ -1,8 +1,9 @@
 ﻿ 
 <?php
 $redis->updateUserStatus($user_id,'');
-			$textMessage = emoji(100086)."抱歉，我不清楚您說什麼，沒辦法回答您可利用下方 '關鍵字' 或\n '類別圖示' 點選需要的服務。".emoji(100094);
- 			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textMessage);  
+		//	$textMessage = emoji(100086)."抱歉，我不清楚您說什麼，沒辦法回答您可利用下方 '關鍵字' 或\n '類別圖示' 點選需要的服務。".emoji(100094);
+			$textMessage = emoji(100086)."抱歉，線上人員忙碌中，沒辦法回答您可利用下方 '關鍵字' 或\n '類別圖示' 點選需要的服務。".emoji(100094);	 
+			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textMessage);  
 	
 			$imagemap= new App\event\message_event\ImagemapHandler;				//create imagemap object
 			$ImageMessageBuilder=$imagemap->createImagemap();
