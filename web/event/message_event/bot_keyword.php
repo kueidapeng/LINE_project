f30b2eaff0c8733335337d2b8ca9a0e53d8bc3b5
Vito <LinWei.Chen@acer.com>
2018-01-12 14:07:26 +0800

    modify button N description

--- web/event/message_event/bot_keyword.php
+++ web/event/message_event/bot_keyword.php
@@ -18,13 +18,9 @@
 					$contents = json_decode(file_get_contents("https://www.cardhoin.com/apiserver/deviceapi/v1/branches/search_t2?latlng=".$latlng."&zip_code=".$zip_code."&keyword=".urlencode($Text)))->result->act006->branches;
 					
 					if(empty($contents)){
-
 						$text = emoji('100095')."很抱歉，找不到您要的優惠。";
 						$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);  
- 
 						$MultiMessageBuilder->add($textMessageBuilder);
- 
-
 					}else{
 					$columns = array();
 					foreach($contents as $content){
@@ -50,29 +46,21 @@
 						//$imagemap='https://maps.googleapis.com/maps/api/staticmap?center='.$content->branch->lat.','.$content->branch->lng.'&zoom=18&sensor=false&scale=1&size=600x300&maptype=roadmap&format=png&markers=size:mid%7Ccolor:0xf896b4%7Clabel:%7C'.$content->branch->lat.','.$content->branch->lng;
 						$title	=mb_strimwidth($content->activities[0]->title, 0, 58, "..."); // max text 120 
 						$column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder(emoji('1F4B3')." ".$content->activities[0]->name." ➤ ".$content->name,$title, $content->brand->logo_img_url, $actions);
-						$columns[] = $column;
-				
-						
+						$columns[] = $column;						
 					}
 		 
 					$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
 					$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(emoji('1F50D')."這訊息要在手機上才能看唷", $carousel);
- 
 					$MultiMessageBuilder->add($msg);
- 
 					}
-
 
 					$actions = array(
 						new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("繼續搜尋", "map_key=Y"),
-						new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("回LINE服務", "map_key=N")
+						new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("回列表", "map_key=N")
 					  );
 					$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder(emoji('1F4CC')." 是否要在搜尋其他優惠？", $actions);
 					$msg2 = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(emoji('1F50D')."這訊息要在手機上才能看唷", $button);
 										
-
 					$MultiMessageBuilder->add($msg2);
 					$bot->replyMessage($reply_token,$MultiMessageBuilder);	
- 
-
 ?>
