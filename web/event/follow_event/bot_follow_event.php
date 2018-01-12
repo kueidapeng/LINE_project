b085c26097f1c7d559ca54455ae91f11390a754c
Vito <LinWei.Chen@acer.com>
2018-01-12 15:32:52 +0800

    modify for chaging UtilityHandler_askAddLocation

--- web/event/follow_event/bot_follow_event.php
+++ web/event/follow_event/bot_follow_event.php
@@ -20,13 +20,9 @@
 			$Text =$displayName.$UtilityHandler->tag_translate($jsonString['bot_follow_event']);	
 					
 			$redis->updateUserStatus($user_id,'no_location');
-			$actions = array(   new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder(emoji('1F4CD')." 定位座標", "line://nv/location")  );
-							   		
-			$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder(emoji('1F50D')." 優惠搜尋","請先點下方選定位座標。", $thumbnailImageUrl,$actions);
-			$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(emoji('1F50D')."這訊息要在手機上才能看唷", $carousel);
-
+		
 			$MultiMessageBuilder = new LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
 			$MultiMessageBuilder->add(new LINE\LINEBot\MessageBuilder\TextMessageBuilder($Text));
-			$MultiMessageBuilder->add($msg);
+			$MultiMessageBuilder->add($UtilityHandler->askAddLocation());   //ask user add location
 			$bot->replyMessage($reply_token, $MultiMessageBuilder);
 ?>			
