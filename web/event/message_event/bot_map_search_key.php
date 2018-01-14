97c678fc52348b28a0cbc65027d8d98f78b0d1fb
Vito <LinWei.Chen@acer.com>
2018-01-14 21:38:32 +0800

    modify for simplify code

--- web/event/message_event/bot_map_search_key.php
+++ web/event/message_event/bot_map_search_key.php
@@ -1,15 +1,10 @@
 <?php
- 
+ use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 			/*
 			location search key message
 			*/
-
- 			
 			$redis->updateUserStatus($user_id,'map_key');
-
+	
 			$text = emoji('100077')."請輸入文字或錄製語音查詢您要的關鍵字。例如：星巴克";
-			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);  
-			$bot->replyMessage($reply_token, $textMessageBuilder);
- 
-
-?>
+	
+			$bot->replyMessage($reply_token, new TextMessageBuilder($text));
