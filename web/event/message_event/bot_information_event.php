1c17ed181e6d5bd620fe01bad5f9276fa9d98942
Vito <LinWei.Chen@acer.com>
2018-01-14 21:26:04 +0800

    modify information for instruction

--- web/event/message_event/bot_information_event.php
+++ web/event/message_event/bot_information_event.php
@@ -4,14 +4,20 @@
     use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
     use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
     use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
-
-    $columns = array();
+    use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+    use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+ 
     $MultiMessageBuilder = new MultiMessageBuilder();
     $policy_url='https://www.cardhoin.com/policy';
     $instagram_url='https://www.instagram.com/cardhoin.app/';
     $facebook_url='https://www.facebook.com/cardhoin/';
-    $baseUrl='https://'. $_SERVER['HTTP_HOST'].getenv('image_path').'certificate_new.png?_ignore=';			
+    //tutor
+    $baseUrl='https://'. $_SERVER['HTTP_HOST'].getenv('image_path').'guide.png?_ignore=';	
+    $actions = array( new MessageTemplateActionBuilder(emoji('1F449')." 使用教學","使用教學") );
+    $column = new CarouselColumnTemplateBuilder('使用教學','卡好用Chat Bot使用步驟教學',$baseUrl, $actions);
+    $columns[] = $column;	
     //policy
+    $baseUrl='https://'. $_SERVER['HTTP_HOST'].getenv('image_path').'certificate_new.png?_ignore=';			
     $actions = array( new UriTemplateActionBuilder(emoji('1F449')." 服務條款連結",$policy_url) );
     $column = new CarouselColumnTemplateBuilder('服務條款','卡好用為宏碁資訊服務股份有限公司所研發...',$baseUrl, $actions);
     $columns[] = $column;	
